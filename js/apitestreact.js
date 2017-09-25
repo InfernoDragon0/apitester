6cf64ae60c5da91e2889cc9c622d5bb516dcf26a
Unknown <calebcheongdnt@gmail.com>
2017-09-25 13:44:04 +0800

    push

--- js/apitestreact.js
+++ js/apitestreact.js
@@ -40,7 +40,7 @@ function fetchToken() {
 $(document).ready(function () {
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
-  writeHistory();  
+  writeHistory();
   var button = document.querySelector('#topupbutton');
   braintree.dropin.create({
     authorization: clientoken,
@@ -50,17 +50,17 @@ $(document).ready(function () {
       instance.requestPaymentMethod(function (requestPaymentMethodErr, payload) {
         // Submit payload.nonce to your server
         console.log("payload is " + payload.nonce);
-  var amount = $('#topupamount').val()
-  
+        var amount = $('#topupamount').val()
+
         var confirm = $('#confirm1').val();
-        
+
         if (confirm == "confirm") {
           if (amount == "") {
             Materialize.toast('You did not enter an amount', 4000)
-            
+
           }
 
-          $.post(self + "/payment", { nonce: payload.nonce, amount: amount}, function (data) {
+          $.post(self + "/payment", { nonce: payload.nonce, amount: amount }, function (data) {
             if (data.includes("Invalid")) {
               Materialize.toast('Error: ' + data, 4000)
             }
@@ -81,53 +81,39 @@ $(document).ready(function () {
 function writeHistory() {
   const DashboardRow = () => (
     <tbody>
-        {datajson.map((data, i) => (
-            <tr key={i}>
-                <td className="transactionid">{data.transactionId}</td>
-                <td>{data.timestamp}</td>
-                <td>{data.amount ? data.amount : data.amount ? data.asset : "--"}</td>
-                <td>{data.asset ? data.asset : "--"}</td>
-                <td>{data.asset2 ? data.asset2 : "--"}</td>
-                <td>{data.$class}</td>
-                {/* <td>{data.resources ? data.$class : "--"}</td> */}
-            </tr>
-        ))}
+      {datajson.map((data, i) => (
+        <tr key={i}>
+          <td className="transactionid">{data.transactionId}</td>
+          <td>{data.timestamp}</td>
+          <td>{data.amount ? data.amount : data.amount ? data.asset : "--"}</td>
+          <td>{data.asset ? data.asset : "--"}</td>
+          <td>{data.asset2 ? data.asset2 : "--"}</td>
+          <td>{data.$class}</td>
+          {/* <td>{data.resources ? data.$class : "--"}</td> */}
+        </tr>
+      ))}
     </tbody>
-)
+  )
 
-ReactDOM.render(
+  ReactDOM.render(
     <div>
-        <br/>
-        <br/>
-        <table class="bordered highlight responsive-table">
-            <thead>
-                <tr>
-                    <th><i className="fa fa-info-circle"></i> Transaction ID </th>
-                    <th className="hidden-xs"><i className="fa fa-clock"></i> Time </th>
-                    <th><i className="fa fa-shopping-cart"></i> Amount </th>
-                    <th><i className="fa fa-user"></i> Client ID </th>
-                    <th><i className="fa fa-user"></i> Merchant ID </th>
-                    <th><i className="fa fa-times"></i> Transaction Type </th>
-                </tr>
-            </thead>
-            <DashboardRow />            
-        </table>
-      
+      <br />
+      <br />
+      <table class="bordered highlight responsive-table">
+        <thead>
+          <tr>
+            <th><i className="fa fa-info-circle"></i> Transaction ID </th>
+            <th className="hidden-xs"><i className="fa fa-clock"></i> Time </th>
+            <th><i className="fa fa-shopping-cart"></i> Amount </th>
+            <th><i className="fa fa-user"></i> Client ID </th>
+            <th><i className="fa fa-user"></i> Merchant ID </th>
+            <th><i className="fa fa-times"></i> Transaction Type </th>
+          </tr>
+        </thead>
+        <DashboardRow />
+      </table>
+
     </div>,
-    document.getElementById("test4"),
-    () => {
-        console.log("rendererd");
-        $("#selectable").selectable({
-            filter: 'tr',
-            stop: function () {
-                var result = ""
-                $(".ui-selected", this).each(function () {
-                    var index = $(this).find(".transactionid").text();
-                    result += ("row " + index + "\n");     
-                })
-                alert("Selected " + result)
-            }
-        });
-    }
-)
+    document.getElementById("test4")
+  )
 }
