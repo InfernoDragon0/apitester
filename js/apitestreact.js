80d37af67c6bc1fc9fb4807ac226e2af45926555
InfernoDragon0 <mushroomapplepiele@gmail.com>
2017-09-25 13:42:20 +0800

    add react history

--- js/apitestreact.js
+++ js/apitestreact.js
@@ -1,6 +1,7 @@
 const self = "http://localhost:7575"
 const payserver = "http://localhost:5000"
 
+
 function openTopupModal() {
   $('#topupmodal').modal('open');
 }
@@ -39,7 +40,7 @@ function fetchToken() {
 $(document).ready(function () {
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
-
+  writeHistory();  
   var button = document.querySelector('#topupbutton');
   braintree.dropin.create({
     authorization: clientoken,
@@ -76,3 +77,57 @@ $(document).ready(function () {
   });
 
 });
+
+function writeHistory() {
+  const DashboardRow = () => (
+    <tbody>
+        {datajson.map((data, i) => (
+            <tr key={i}>
+                <td className="transactionid">{data.transactionId}</td>
+                <td>{data.timestamp}</td>
+                <td>{data.amount ? data.amount : data.amount ? data.asset : "--"}</td>
+                <td>{data.asset ? data.asset : "--"}</td>
+                <td>{data.asset2 ? data.asset2 : "--"}</td>
+                <td>{data.$class}</td>
+                {/* <td>{data.resources ? data.$class : "--"}</td> */}
+            </tr>
+        ))}
+    </tbody>
+)
+
+ReactDOM.render(
+    <div>
+        <br/>
+        <br/>
+        <table class="bordered highlight responsive-table">
+            <thead>
+                <tr>
+                    <th><i className="fa fa-info-circle"></i> Transaction ID </th>
+                    <th className="hidden-xs"><i className="fa fa-clock"></i> Time </th>
+                    <th><i className="fa fa-shopping-cart"></i> Amount </th>
+                    <th><i className="fa fa-user"></i> Client ID </th>
+                    <th><i className="fa fa-user"></i> Merchant ID </th>
+                    <th><i className="fa fa-times"></i> Transaction Type </th>
+                </tr>
+            </thead>
+            <DashboardRow />            
+        </table>
+      
+    </div>,
+    document.getElementById("test4"),
+    () => {
+        console.log("rendererd");
+        $("#selectable").selectable({
+            filter: 'tr',
+            stop: function () {
+                var result = ""
+                $(".ui-selected", this).each(function () {
+                    var index = $(this).find(".transactionid").text();
+                    result += ("row " + index + "\n");     
+                })
+                alert("Selected " + result)
+            }
+        });
+    }
+)
+}
