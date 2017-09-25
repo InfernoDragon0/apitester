6410cf2bf5cbf7c0c4d4910989808a5b7490fa23
Unknown <calebcheongdnt@gmail.com>
2017-09-25 13:34:54 +0800

    push

--- nodemodjs/requestwrite.js
+++ nodemodjs/requestwrite.js
@@ -1,5 +1,6 @@
 const walletServer = 5001
 const paymentServer = 'http://localhost:5000'
+const request = require('request')
 const cvars = require('./commonvariables.js')
 
 module.exports.BTTokenAuth = BTTokenAuth
@@ -16,14 +17,13 @@ function BTTokenAuth(token, res,page) {
 function getHistory(res, page, clientoken) {
     request.post(paymentServer + '/customerhistory', {
         form: {
-            "clientID": 123
+            "clientID": 3
         }
     }, function (error, response, body) {
         if (error) {
             console.log('error:', error); // Print the error if one occurred 
             return;
         }
-        console.log(response)
         console.log(body)
         res.render(page,
             {
